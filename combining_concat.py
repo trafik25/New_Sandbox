60baf4200e38696127b59cf01f16b72b23d95c96
Thomas Howard <thomashoward206@gmail.com>
2022-04-15 17:21:32 -0700

    1.2

--- combining_concat.py
+++ combining_concat.py
@@ -4,3 +4,13 @@ last_name = 'howard'
 full_name = first_name + ' ' + last_name
 print(full_name.upper())
 print("Hello" + ', ' + full_name.title())
+
+
+
+message = "Hello" + ', ' + full_name.title() + '!'
+print(message)
+
+print("python")
+print("\tpython") #whitespace \t 
+print("Languages:\nPython\njava\nPascal") #new line \n
+
