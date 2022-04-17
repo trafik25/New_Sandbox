571286c34c39139671043c38a650e18ec8a2596a
Thomas Howard <thomashoward206@gmail.com>
2022-04-17 11:49:30 -0700

    1.2

---
+++ New 1/rstrip.py
@@ -0,0 +1,11 @@
+random_string = '   this is good '
+
+# Leading whitepsace are removed
+print(random_string.lstrip())
+
+fav_language = "  python"
+print(fav_language.lstrip())
+
+fav_language = "python    "
+print(fav_language.rstrip())
+
