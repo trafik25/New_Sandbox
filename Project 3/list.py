bcf65ed991d8abc9f24b08bc1a88fd0ddfa0b176
Thomas Howard <thomashoward206@gmail.com>
2022-04-17 12:09:29 -0700

    1.5

---
+++ Project 3/list.py
@@ -0,0 +1,29 @@
+
+
+# not pretty but it gets the job done
+bicycles = ['cannondale', 'trek', 'redline', 'specialized']
+print(bicycles)
+
+#better
+bicycles = ['cannondale', 'trek', 'redline', 'specialized']
+print(bicycles[0])
+
+#title in a list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+
+#upper in a list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].upper())
+
+#accessing values in a list by index number
+print("XXXXXXXXXXXXXXXXX")
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1])
+print(bicycles[2])
+
+#accessing the last value in a list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])
+
+
---
+++ Project 3/valuesinalist.py
@@ -0,0 +1,8 @@
+
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bike was a " + bicycles[1].title()
+print(message)
+
+
+
